82dfae541cd7712254bef207365b75c0e6667a7e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 18:20:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if(text.match(/\b[A-Z].*\W\b/))
+if(text.scan(/\b[A-Z].*\W\b/))
   return true
 else
   return false
