0b3f622fecd7c52f38f630bbe5322bb2238b8fc5
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 17:04:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if(word.match(/\b[AEIOUaeiou][a-z]*\b/))
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b(un)*(ing)\b/)
 end
 
 def words_five_letters_long(text)
@@ -15,5 +19,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+phone.scan(/(\d+)-(\d+)-(\d+)/)
 end
