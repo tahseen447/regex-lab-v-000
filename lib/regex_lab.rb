32648547d36e106ba927f3ff2cd0bb877bed1c4a
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 17:32:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\A(un).(ing)\\z/)
+  text.scan(/\b(un).?(ing)\z/)
 end
 
 def words_five_letters_long(text)
