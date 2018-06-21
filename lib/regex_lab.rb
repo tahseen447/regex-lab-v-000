1d72ec254314af4015124848df50e975e51f96cc
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-21 18:18:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b(un).?(ing)\z/)
+  text.scan(/\b(un).?(ing)\b/)
 end
 
 def words_five_letters_long(text)
@@ -15,7 +15,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if(text.match(/\b[A-Z]/))
+if(text.match(/\b[A-Z].*\W\b/))
 end
 end
 
